a02fe1931a0e58b88a345f49702893721c1ee7e2
Dertyos <juliansalcedo4@gmail.com>
2022-09-17 02:38:34 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -4,30 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import { Generator } from "webpack";
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".org", ".net", ".tv"];
+
+function domainGenerator() {
+  var domain = "";
+  for (let x of pronoun) {
+    for (let j of adj) {
+      for (let a of noun) {
+        for (let i of extension) {
+          domain += "<p>" + x + j + a + i + "</p>";
+        }
+      }
+    }
+  }
+  return domain;
+}
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [".com", ".org", ".net", ".tv"];
-
-  function randomBetween(min, max) {
-    // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  function domainGenerator() {
-    let p = pronoun[randomBetween(1, 2) - 1];
-    let a = adj[randomBetween(1, 2) - 1];
-    let n = noun[randomBetween(1, 2) - 1];
-    let e = extension[randomBetween(1, 4) - 1];
-
-    let domain = p + a + n + e;
-    return domain;
-  }
-  document.getElementById("generator").innerHTML = domainGenerator();
+  document.querySelector("#generator").innerHTML = domainGenerator();
 };
